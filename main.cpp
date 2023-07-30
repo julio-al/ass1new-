9dd5de9524d0ad846779f92d7e09d1ad419d79ed
julio-al <julioc-alves@hotmail.com>
2023-07-30 13:44:38 +0000

    1version commit message
    Signed-off-by: julio-al <julioc-alves@hotmail.com>

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
+
+
 #include "mbed.h"
 
 // main() runs in its own thread in the OS
+InterruptIn bttn (PC_13);
+
+void bttn_interrupt(){
+    printf("Buttn pressed\r\n");
+}
+
 int main()
 {
+    bttn.fall(&bttn_interrupt);
     while (true) {
 
     }
