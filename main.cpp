ff33bea5af9d122ea633602283cd7f000cf625c3
julio-al <julioc-alves@hotmail.com>
2023-08-06 17:33:23 +0000

    lab1commit1
    Signed-off-by: julio-al <julioc-alves@hotmail.com>

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
-
+// what is the error message ?
+is a HTTP response status code indicating that the requested resource has been temporarily moved to another URI.
+//  what does that mean ?
+that indicates that you are trying to use a mutex in an interrupt service routine , wich is not allowed in mbed os .
 
 #include "mbed.h"
 
